4bacf4807e450dda91f5a04fbb5311bf3a848301
Gamra01 <ilyassstudies2017@gmail.com>
2021-05-20 16:01:38 +0200

    time

--- Server/round.py
+++ Server/round.py
@@ -1,56 +1,59 @@
 import time as t
 from _thread import *
-from .game import Game
-from .chat import Chat
+from chat import Chat
 
 
 class Round(object):
-    def __init__(self, word, player_drawing, players):
-        """
-        initialization d'objet
-        :param word: str
-        :param player_drawing: Player
-        :param players: Player[]
-        """
+    def __init__(self, word, player_drawing, game):
         self.word = word
         self.player_drawing = player_drawing
         self.player_guessed = []
         self.skips = 0
-        self.player_scores = {player:0 for player in players}
         self.time = 75
+        self.game = game
+        self.player_scores = {player: 0 for player in self.game.players}
         self.chat = Chat(self)
         start_new_thread(self.time_thread, ())
 
+    def skip(self):
+        self.skips += 1
+        if self.skips > len(self.game.players) - 2:
+            return True
+
+        return False
+
+    def get_scores(self):
+        return self.player_scores
+
+    def get_score(self, player):
+        if player in self.player_scores:
+            return self.player_scores[player]
+        else:
+            raise Exception("Player not in score list")
+
     def time_thread(self):
-        """
-        Runs in thread to keep track of time
-        :return: None
-        """
         while self.time > 0:
             t.sleep(1)
             self.time -= 1
         self.end_round("Time is up")
 
     def guess(self, player, wrd):
-        """
-        :returns bool if player got guess correct
-        :param player: Player
-        :param wrd: str
-        :return: bool
-        """
+
         correct = wrd == self.word
         if correct:
             self.player_guessed.append(player)
-            # TODO implementer le score systeme
+            # TODO implement scoring system here
+            self.chat.update_chat(f"{player.name} has guessed the word.")
+            return True
+
+        self.chat.update_chat(f"{player.name} guessed {wrd}")
+        return False
 
     def player_left(self, player):
         """
-        
-        supprime le joueur qui est parti des scores et de la liste  
-        :param player: Player
-        :return: None
+        retirer le joueur qui a quitté la partie et enlever le du score et liste
+
         """
-        # might not be able to use player as key in dict
         if player in self.player_scores:
             del self.player_scores[player]
 
@@ -58,8 +61,11 @@ class Round(object):
             self.player_guessed.remove(player)
 
         if player == self.player_drawing:
+            self.chat.update_chat("Round has been skipped because the drawer left.")
             self.end_round("Drawing player leaves")
 
     def end_round(self, msg):
-        # TODO implementer la function end_round
-        pass
+        for player in self.game.players:
+            if player in self.player_scores:
+                player.update_score(self.player_scores[player])
+        self.game.round_ended()
