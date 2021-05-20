1c0b8ec060ccb92e725928aa6544d6106aa3ce54
Gamra01 <ilyassstudies2017@gmail.com>
2021-05-20 18:24:21 +0200

    server side finished

--- Server/chat.py
+++ Server/chat.py
@@ -20,3 +20,23 @@ class Chat(object):
 
     def __repr__(self):
         return str(self)
+class Chat(object):
+
+    def __init__(self, r):
+        self.content = []
+        self.round = r
+
+    def update_chat(self, msg):
+        self.content.append(msg)
+
+    def get_chat(self):
+        return self.content
+
+    def __len__(self):
+        return len(self.content)
+
+    def __str__(self):
+        return "".join(self.content)
+
+    def __repr__(self):
+        return str(self)
